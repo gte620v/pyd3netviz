6fd930ac8eb6058c028cde05d7b8fd964000af8e
gte620v <bob.baxley@gmail.com>
2017-01-29 20:07:04 -0500

    adding js files to distribution

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 """pyd3netviz"""
 
-from distutils.core import setup
+from setuptools import setup
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
 setup(
     name='pyd3netviz',
     packages=['pyd3netviz'],
-    version='0.1',
+    package_data={
+        'pyd3netviz': ['js/*.js'],
+    },
+    version='0.1.2',
     description='Simple wrapper around d3 force graph for python and Jupyter Notebooks',
     author='Bob Baxley',
     author_email='bob.baxley@gmail.com',
+    license='MIT',
     url='https://github.com/gte620v/pyd3netviz',
-    download_url='https://github.com/gte620v/pyd3netviz/archive/0.1.tar.gz',
     keywords=['network graph', 'd3js', 'visualization',
               'force layout', 'Jupyter Notebook'],
     classifiers=[],
-    install_requires=required,
+    install_requires=['pyperclip==1.5.27', 'networkx==1.11', ],
 )
